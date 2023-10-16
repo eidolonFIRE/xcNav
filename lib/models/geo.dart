f1a6d664ba2ff4b0c6f685e802931207f71331e3
Caleb Johnson <c4leb.johnson@gmail.com>
2023-10-16 08:09:19 -0700

    vario more sensitive

--- lib/models/geo.dart
+++ lib/models/geo.dart
@@ -115,7 +115,7 @@ class Geo {
       vario = (alt - prev.alt) / (time - prev.time) * 1000;
       if (vario.isNaN || vario.isInfinite) vario = 0;
       // Blend vario with previous vario reading to offer some mild smoothing
-      if (prev.varioSmooth.isFinite && vario.abs() < 99) varioSmooth = vario * 0.1 + prev.varioSmooth * 0.9;
+      if (prev.varioSmooth.isFinite && vario.abs() < 99) varioSmooth = vario * 0.2 + prev.varioSmooth * 0.8;
       spdSmooth = spd * 0.1 + prev.spdSmooth * 0.9;
     } else {
       vario = prev?.vario ?? 0;
