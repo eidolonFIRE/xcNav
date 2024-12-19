49d616661f401e3f0bccd052851eab7511de7c13
Caleb Johnson <c4leb.johnson@gmail.com>
2024-12-18 21:25:56 -0800

    fixes #204, ppg.report units

--- lib/screens/weather_viewer.dart
+++ lib/screens/weather_viewer.dart
@@ -3,6 +3,8 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:xcnav/providers/my_telemetry.dart';
+import 'package:xcnav/settings_service.dart';
+import 'package:xcnav/units.dart';
 
 class WeatherViewer extends StatefulWidget {
   const WeatherViewer({super.key});
@@ -30,8 +32,16 @@ class _WeatherViewerState extends State<WeatherViewer> {
             },
           ),
         );
-      controller.loadRequest(Uri.parse("https://ppg.report/${myTelemetry.geo?.lat},${myTelemetry.geo?.lng}"),
-          headers: {"xcNav": "${version.version}  -  ( build ${version.buildNumber}"});
+      // Some fixes to map to ppg.report
+      String distCoarse = getUnitStr(UnitType.distCoarse);
+      if (distCoarse == "mi") distCoarse = "miles";
+      String speed = getUnitStr(UnitType.speed);
+      if (speed == "kts") speed = "knots";
+      final String uri =
+          "https://ppg.report/${myTelemetry.geo?.lat},${myTelemetry.geo?.lng}#user-speed-unit=$speed&user-distance-unit=$distCoarse&user-height-unit=${getUnitStr(UnitType.distFine)}&user-altitude=${settingsMgr.primaryAltimeter.value.name.toUpperCase()}";
+      debugPrint(uri);
+      controller
+          .loadRequest(Uri.parse(uri), headers: {"xcNav": "${version.version}  -  ( build ${version.buildNumber}"});
     });
   }
 
